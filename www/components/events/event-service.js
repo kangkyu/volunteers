b94529ec991e1f6e172c81eef21740d736f7da9e
kangkyu <kangkyu1111@gmail.com>
2015-02-23 15:15:49 -0800

    link server url to event list

--- www/components/events/event-service.js
+++ www/components/events/event-service.js
@@ -3,26 +3,28 @@ angular
 .factory('eventService',['$http',
 function($http){
     var eventService = {};
+    // var baseUrl = 'http://localhost:3000/api/events';
+    var baseUrl = 'http://104.236.237.119:3000/api/events'
 
     eventService.loadAll = function(){
-        return $http.get('http://localhost:3000/api/events');
+        return $http.get(baseUrl);
     };
 
     eventService.loadById = function(id){
-        return $http.get('http://localhost:3000/api/events' + '/' + id);
+        return $http.get(baseUrl + '/' + id);
     };
 
     eventService.deleteEvent = function(id){
-        return $http.delete('http://localhost:3000/api/events' + '/' + id);
+        return $http.delete(baseUrl + '/' + id);
     };
 
 
     eventService.addEvent = function(event){
-        return $http.post('http://localhost:3000/api/events', event);
+        return $http.post(baseUrl, event);
     };
 
     eventService.updateEvent = function(id, event){
-        return $http.put('http://localhost:3000/api/events' + '/'+ id, event);
+        return $http.put(baseUrl + '/'+ id, event);
     };
 
     return eventService;
