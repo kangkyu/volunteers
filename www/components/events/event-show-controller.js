ba27f0f6c5af59bf2d441e7118a3292e1fbd4677
kangkyu <kangkyu1111@gmail.com>
2015-02-25 06:53:37 -0800

    edit event view and controller to take a picture for each event

--- www/components/events/event-show-controller.js
+++ www/components/events/event-show-controller.js
@@ -1,7 +1,7 @@
 angular
 .module('eventShowCtrlModule', [])
-.controller('eventShowCtrl',['$scope', '$stateParams', 'eventService',
-function($scope, $stateParams, eventService){
+.controller('eventShowCtrl',['$scope', '$stateParams', 'eventService', 'cameraService',
+function($scope, $stateParams, eventService, cameraService){
 
     eventService.loadById($stateParams.eventId).success(function(data){
         $scope.event = data;
@@ -12,6 +12,25 @@ function($scope, $stateParams, eventService){
             eventService.loadAll().success(function(data){
                 $scope.events = data;
             });
+        });
+    };
+
+    $scope.getEventPhoto = function(event){
+        cameraService.getPicture(options).then(function(imageData) {
+
+            console.log(imageData);
+            event.lastPhotoURI = "data:image/jpeg;base64," + imageData;
+
+            // var image = document.getElementById('myImage');
+            // image.src = "data:image/jpeg;base64," + imageData;
+
+        }, function(err) {
+            console.err(err);
+        });
+
+        eventService.updateEvent(event._id, event).success(function(data){
+            $scope.event = data;
+            console.log($scope.event);
         });
     };
 }]);
